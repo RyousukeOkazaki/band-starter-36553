2c3d5ac9fe27d7071fc9040d39bb2fc64052ff7b
RyousukeOkazaki <soratobuowan@gmail.com>
2021-09-15 22:28:30 +0900

    ルーム作成機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_074505) do
+ActiveRecord::Schema.define(version: 2021_09_15_123156) do
 
   create_table "as_leaders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "what_band", null: false
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2021_09_15_074505) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
